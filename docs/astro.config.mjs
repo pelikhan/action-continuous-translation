40d253c042348a0d4ec991a2c1a51cdae98d1f34
Felix Schneider <99918022+trueberryless@users.noreply.github.com>
2025-07-06 19:14:41 +0200

    feat: integrate lunaria and update docs (#35)
    Co-authored-by: Peli de Halleux <pelikhan@users.noreply.github.com>

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import starlightLinksValidator from "starlight-links-validator";
+import lunaria from '@lunariajs/starlight';
 import { title } from "./resources.json" assert { type: "json" };
 
 // https://astro.build/config
@@ -11,6 +12,9 @@ export default defineConfig({
   integrations: [
     starlight({
       plugins: [
+        lunaria({
+          sync: true,
+        }),
         starlightLinksValidator({
           errorOnRelativeLinks: false,
           errorOnInconsistentLocale: true,
